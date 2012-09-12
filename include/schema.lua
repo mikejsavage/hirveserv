26c5b63a9406265bfd3bd07b49b50b81814d8d59
Michael Savage <mikejsavage@gmail.com>
2012-09-12 17:46:56 +0100

    Separate schemas into their own files

--- include/schema.lua
+++ include/schema.lua
@@ -1,35 +1,15 @@
-local function createUsers( db )
-	db( [[CREATE TABLE IF NOT EXISTS users (
-		userid INTEGER PRIMARY KEY,
-		name STRING UNIQUE,
-		password STRING
-	)]] )()
+return setmetatable( { }, {
+	__index = function( self, key )
+		local schema = assert( io.contents( "schema/%s.sql" % key ) )
 
-	db( [[CREATE TABLE IF NOT EXISTS privs (
-		userid INTEGER,
-		priv STRING
-	)]] )()
+		return function( db )
+			for query in schema:gmatch( "([^;]+)" ) do
+				query = query:trim()
 
-	db( [[CREATE TABLE IF NOT EXISTS settings (
-		userid INTEGER,
-		setting STRING,
-		value STRING
-	)]] )()
-
-	db( [[CREATE TABLE IF NOT EXISTS ipauths (
-		userid INTEGER,
-		ip STRING
-	)]] )()
-
-	db( [[CREATE INDEX IF NOT EXISTS idx_privs ON privs ( userid )]] )()
-	db( [[CREATE INDEX IF NOT EXISTS idx_settings ON settings ( userid )]] )()
-	db( [[CREATE INDEX IF NOT EXISTS idx_ipauths ON ipauths ( userid, ip )]] )()
-end
-
-local function createLogs( db )
-end
-
-return {
-	users = createUsers,
-	logs = createLogs,
-}
+				if query ~= "" then
+					db( query )()
+				end
+			end
+		end
+	end,
+} )
---
+++ schema/users.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS users (
+	userid INTEGER PRIMARY KEY,
+	name STRING UNIQUE,
+	password STRING
+);
+
+CREATE TABLE IF NOT EXISTS privs (
+	userid INTEGER,
+	priv STRING
+);
+
+CREATE TABLE IF NOT EXISTS settings (
+	userid INTEGER,
+	setting STRING,
+	value STRING
+);
+
+CREATE TABLE IF NOT EXISTS ipauths (
+	userid INTEGER,
+	ip STRING
+);
+
+CREATE INDEX IF NOT EXISTS idx_privs ON privs ( userid );
+CREATE INDEX IF NOT EXISTS idx_settings ON settings ( userid );
+CREATE INDEX IF NOT EXISTS idx_ipauths ON ipauths ( userid, ip );
+
