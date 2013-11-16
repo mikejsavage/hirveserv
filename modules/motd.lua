8cea49123df81b9e476df00c5b23527e19984c24
Michael Savage <mikejsavage@gmail.com>
2013-11-16 12:41:00 +0000

    Add editmotd command

--- modules/motd.lua
+++ modules/motd.lua
@@ -6,16 +6,7 @@ local function sendMotd( client )
 	end
 end
 
-chat.command( "motd", nil, sendMotd, "Show message of the day" )
-
-chat.command( "setmotd", "user", function( client, args )
-	if args == "" then
-		client:msg( "Blank MOTD = boring." )
-		return
-	end
-
-	local newMotd = args:gsub( "\\n", "\n" )
-
+local function updateMotd( client, newMotd )
 	local file = assert( io.open( "data/motd.txt", "w" ) )
 	file:write( newMotd )
 	file:close()
@@ -23,6 +14,41 @@ chat.command( "setmotd", "user", function( client, args )
 	motd = newMotd
 
 	chat.msg( "#ly%s#lw sounds the MOTD alarm! %s", client.name, newMotd )
+end
+
+chat.command( "motd", nil, sendMotd, "Show message of the day" )
+
+chat.command( "setmotd", "user", function( client, args )
+	if args == "" then
+		client:msg( "Blank MOTD = boring." )
+
+		return
+	end
+
+	local newMotd = args:gsub( "\\n", "\n" )
+
+	updateMotd( client, newMotd )
 end, "Update MOTD" )
+
+chat.handler( "editmotd", { "editor" }, function( client )
+	client:pushHandler( "editor", motd )
+
+	local _, newMotd = coroutine.yield()
+
+	if not newMotd or newMotd == "" then
+		if newMotd == "" then
+			client:msg( "Blank MOTD = boring." )
+		end
+
+		return
+	end
+
+	updateMotd( client, newMotd )
+end )
+
+chat.command( "editmotd", "user", function( client )
+	client:pushHandler( "editmotd" )
+end, "Update MOTD using the editor" )
+
 
 chat.listen( "connect", sendMotd )
