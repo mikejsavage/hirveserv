2363bf391a13bd50b100dfeef3ae757a31bca819
Michael Savage <mikejsavage@gmail.com>
2013-12-23 14:43:44 +0000

    Make compact save its settings and be even more compact

--- modules/compact.lua
+++ modules/compact.lua
@@ -1,13 +1,14 @@
 chat.listen( "chat", function( from, message, recipients )
 	for client, message in pairs( recipients ) do
 		if client.user and client.user.settings.compact then
-			recipients[ client ] = "\n" .. message:trim()
+			recipients[ client ] = message:trim()
 		end
 	end
 end )
 
 chat.command( "compact", "user", function( client )
 	client.user.settings.compact = not client.user.settings.compact
+	client.user:save()
 
 	client:msg( "Compact chat %sabled.", client.user.settings.compact and "en" or "dis" )
 end, "Toggle compact chat" )
