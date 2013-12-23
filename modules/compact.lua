b654505c315ddaa9efcc83a89a6feb8d0e6b1463
Michael Savage <mikejsavage@gmail.com>
2013-12-23 14:27:22 +0000

    Add compact module

---
+++ modules/compact.lua
@@ -0,0 +1,13 @@
+chat.listen( "chatAll", function( from, message, recipients )
+	for client, message in pairs( recipients ) do
+		if client.user and client.user.settings.compact then
+			recipients[ client ] = "\n" .. message:trim()
+		end
+	end
+end )
+
+chat.command( "compact", "user", function( client )
+	client.user.settings.compact = not client.user.settings.compact
+
+	client:msg( "Compact chat %sabled.", client.user.settings.compact and "en" or "dis" )
+end, "Toggle compact chat" )
