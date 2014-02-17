290ed5d022b31a1a46c1065220b1f0b40ec42a7b
Michael Savage <mikejsavage@gmail.com>
2014-02-17 15:20:31 +0000

    Fix pattern in MMClient.accept
    Format is xxx.xxx.xxx.xxxPPPPP where x can be a number, meaning reading
    up to five consequtive numbers is not going to be reading their entire
    connection string.

--- protocols/mm.lua
+++ protocols/mm.lua
@@ -74,7 +74,7 @@ end
 local _M = { client = MMClient }
 
 function _M.accept( client )
-	local name, len = client.dataBuffer:match( "^CHAT:(.-)\n.-[%d ][%d ][%d ][%d ][%d ]()" )
+	local name, len = client.dataBuffer:match( "^CHAT:(.-)\n.+[%d ][%d ][%d ][%d ][%d ]()" )
 
 	if not name then
 		return false
