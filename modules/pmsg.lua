1f58d9b2988c748d41a8a97b03ad44daa4d803d9
Michael Savage <mikejsavage@gmail.com>
2014-02-23 15:23:24 +0000

    Add qpmsg

--- modules/pmsg.lua
+++ modules/pmsg.lua
@@ -1,5 +1,5 @@
-chat.command( "pmsg", nil, {
-	[ "^(%S+)%s+(.+)$" ] = function( client, name, message )
+local function sendPM( silent )
+	return function( client, name, message )
 		local target = chat.clientFromName( name )
 
 		if target then
@@ -8,9 +8,20 @@ chat.command( "pmsg", nil, {
 			end
 
 			target:msg( "PM from #ly%s#lw:#d %s", client.name, message )
-			client:msg( "PM to #ly%s#lw:#d %s", target.name, message )
+
+			if not silent then
+				client:msg( "PM to #ly%s#lw:#d %s", target.name, message )
+			end
 		else
 			client:msg( "There's nobody called #ly%s#lw.", name )
 		end
-	end,
+	end
+end
+
+chat.command( "pmsg", nil, {
+	[ "^(%S+)%s+(.+)$" ] = sendPM( false ),
 }, "<name> <message>", "Send a private message" )
+
+chat.command( "qpmsg", "user", {
+	[ "^(%S+)%s+(.+)$" ] = sendPM( true ),
+}, "<name> <message>", "Send a private message that doesn't rep to you" )
