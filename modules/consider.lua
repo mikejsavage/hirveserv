8dd4fe0c7d92dcb6a0a57de06bfd09ace0346730
Michael Savage <mikejsavage@gmail.com>
2014-03-18 22:08:27 +0000

    Add consider module

---
+++ modules/consider.lua
@@ -0,0 +1,98 @@
+local lfs = require( "lfs" )
+
+local MaxResults = 10
+
+local considers = { }
+
+lfs.mkdir( "data/consider" )
+
+local colours = {
+	acid = "#g",
+	cold = "#lb",
+	evil = "\27[1;30m",
+	fire = "#lr",
+	good = "#lg",
+	holy = "#lw",
+	lightning = "#ly",
+	poison = "#y",
+	pressure = "#lc",
+}
+
+local function niceConsider( consider )
+	local output = { "#lw%s #ly[%s]" % { consider.name, consider.zone } }
+
+	if consider.align then
+		table.insert( output, "#d" .. consider.align )
+	end
+
+	if consider.strong then
+		table.insert( output, "#dStrong against: " .. consider.strong )
+	end
+
+	if consider.weak then
+		table.insert( output, "#dWeak against: " .. consider.weak )
+	end
+
+	table.insert( output, "" )
+
+	return table.concat( output, "\n" )
+end
+
+local function addResists( consider, key, line )
+	local resists = { }
+
+	for resist in line:gmatch( "(%u%u+)" ) do
+		table.insert( resists, colours[ resist:lower() ] .. resist )
+	end
+
+	consider[ key ] = table.concat( resists, " " )
+end
+
+for f in lfs.dir( "data/consider" ) do
+	if f:match( "%.txt$" ) then
+		local zone = assert( io.contents( "data/consider/" .. f ) )
+		local name, mobs = zone:match( "^([^\n]+)\n\n(.+)$" )
+
+		assert( name, "Bad zone: " .. f )
+
+		for mob in ( mobs .. "\n\n" ):gmatch( "(.-)\n\n" ) do
+			local consider = { zone = name }
+
+			for line in mob:gmatch( "([^\n]+)" ) do
+				if not consider.name then
+					consider.name = line
+				else
+					if line:match( "^Strong against:" ) then
+						addResists( consider, "strong", line )
+					elseif line:match( "^Weak against:" ) then
+						addResists( consider, "weak", line )
+					else
+						consider.align = line
+					end
+				end
+			end
+
+			assert( consider.name, "No name in: " .. mob )
+
+			table.insert( considers, consider )
+		end
+	end
+end
+
+chat.command( "consider", "user", function( client, name )
+	local output = { "Searching considers for #lm%s#lw:" % name }
+
+	for _, consider in ipairs( considers ) do
+		if consider.name:find( name, 1, true ) then
+			if #output >= MaxResults then
+				table.insert( output, "#lwToo many results. Try to be more specific!" )
+
+				break
+			end
+
+			table.insert( output, niceConsider( consider ) )
+		end
+	end
+
+	client:msg( output )
+end, "Search consider database" )
