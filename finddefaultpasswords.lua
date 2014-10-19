23020229806d32e62bc332adf24d0709c5170549
Michael Savage <mikejsavage@gmail.com>
2014-10-19 22:05:53 +0100

    Add a script to flag accounts with a default password

---
+++ finddefaultpasswords.lua
@@ -0,0 +1,32 @@
+#! /usr/bin/lua
+
+local json = require( "cjson" )
+local lfs = require( "lfs" )
+local bcrypt = require( "bcrypt" )
+
+function io.readFile( path )
+	local file = assert( io.open( path, "r" ) )
+	local contents = assert( file:read( "*a" ) )
+	assert( file:close() )
+
+	return contents
+end
+
+local words_file = io.readFile( "include/words.lua" )
+local words = { }
+
+for word in words_file:gmatch( "\"(.-)\"" ) do
+	table.insert( words, word )
+end
+
+for file in lfs.dir( "data/users" ) do
+	if file:match( "%.json$" ) then
+		local user = json.decode( io.readFile( "data/users/" .. file ) )
+
+		for _, word in ipairs( words ) do
+			if bcrypt.verify( word, user.password ) then
+				print( file )
+			end
+		end
+	end
+end
