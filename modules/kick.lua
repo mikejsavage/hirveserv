f3e9d15fed23035f8a51829b0178a758ae18551e
Michael Savage <mikejsavage@gmail.com>
2019-02-28 17:49:56 +0200

    Kick reason

--- modules/kick.lua
+++ modules/kick.lua
@@ -1,11 +1,18 @@
 chat.command( "kick", "kick", {
-	[ "^(%S+)$" ] = function( client, name )
+	[ "^(%S+)%s*(.-)$" ] = function( client, name, reason )
 		local target = chat.clientFromName( name )
 
 		if target then
+			if reason ~= "" then
+				target:msg( "#lm%s#lw get stitches.", reason )
+			end
 			target:kill()
 
-			chat.msg( "#ly%s#lw was kicked by #ly%s#d.", target.name, client.name )
+			if reason ~= "" then
+				chat.msg( "#ly%s#lw was kicked by #ly%s#lw for #lm%s#lw.", target.name, client.name, reason )
+			else
+				chat.msg( "#ly%s#lw was kicked by #ly%s#lw.", target.name, client.name )
+			end
 		else
 			client:msg( "There's nobody called #ly%s#d.", name )
 		end
